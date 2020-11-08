5f094b46c2555aea0cb6045b99500d5c1ad3e09a
Peter Kajan <sknitro@gmail.com>
2020-11-08 13:08:27 +0100

    create db
    create db if not exist

--- index.php
+++ index.php
@@ -1,40 +1,55 @@
 <?php
-
-$json_data  = json_decode(file_get_contents("settings.json"), true);
-$dbFileName = $json_data['dbFileName'];
-$tableName  = $json_data['tableName'];
-$linkURL    = $json_data['linkURL'];
-$pageTitle  = $json_data['pageTitle'];
-$db         = new SQLite3($dbFileName);
+$filename = "settings.json";
+if (file_exists($filename)) {
 
 
-header("Content-type: text/xml");
-echo "<?xml version='1.0' encoding='UTF-8'?>
-<rss version='2.0'>
-<channel>
-<title>$pageTitle</title>
-<description>Torrents links to download</description>
-<link>$linkURL</link>
-<item>
-<title>NULL</title>
-<link></link>
-<pubDate>Mon, 01 Jan 2020 00:00:00 +0200</pubDate>
-</item>";
+    $json_data  = json_decode(file_get_contents($filename), true);
+    $dbFileName = $json_data['dbFileName'];
+    $tableName  = $json_data['tableName'];
+    $linkURL    = $json_data['linkURL'];
+    $pageTitle  = $json_data['pageTitle'];
+    $db         = new SQLite3($dbFileName);
 
-$tmpArray = [];
+    $db->exec("CREATE TABLE $tableName(id INTEGER PRIMARY KEY UNIQUE, title VARCHAR (250) NOT NULL, link VARCHAR (2500) NOT NULL, pubDate DATETIME NOT NULL)");
 
-$res = $db->query("SELECT * FROM $tableName");
-while ($data = $res->fetchArray()) {
-    $title   = htmlspecialchars($data["title"]);
-    $link    = htmlspecialchars($data["link"]);
-    $pubDate = htmlspecialchars($data["pubDate"]);
-    
-    $string = "<item>
+    header("Content-type: text/xml");
+    echo "<?xml version='1.0' encoding='UTF-8'?>
+            <rss version='2.0'>
+            <channel>
+            <title>$pageTitle</title>
+            <description>Torrents links to download</description>
+            <link>$linkURL</link>
+            <item>
+            <title>NULL</title>
+            <link></link>
+            <pubDate>Mon, 01 Jan 2020 00:00:00 +0200</pubDate>
+            </item>";
+
+    $tmpArray = [];
+
+    $res = $db->query("SELECT * FROM $tableName");
+    while ($data = $res->fetchArray()) {
+        $title   = htmlspecialchars($data["title"]);
+        $link    = htmlspecialchars($data["link"]);
+        $pubDate = htmlspecialchars($data["pubDate"]);
+
+        $string = "<item>
         <title>$title</title>
         <link>$link</link>
         <pubDate>$pubDate</pubDate>
     </item>";
-    array_unshift($tmpArray, $string);
+        array_unshift($tmpArray, $string);
+    }
+    echo implode(" ", $tmpArray);
+    echo "</channel></rss>";
+} else {
+    echo "The file $filename does not exist";
+    echo '<pre>
+    {
+        "dbFileName": "database.db",
+        "tableName": "webfeed",
+        "linkURL": "https://myhosting.eu",
+        "pageTitle": "my RSS",
+        "pwd_hash": "sha256_hash_of_your_password"
+    }</pre>';
 }
-echo implode(" ",$tmpArray);
-echo "</channel></rss>";
