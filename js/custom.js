8d1e1ce16fd8d2aacd3768db43817598cfe5d183
Peter Kajan <sknitro@gmail.com>
2024-11-19 13:26:52 +0100

    decode all encoded characters

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,6 @@ function addMagnetName() {
 	let magnetLink = document.getElementById("linkForm_link").value;
 	let magnetName = document.getElementById("linkForm_name").value;
 	if (magnetLink && !magnetName) {
-		document.getElementById("linkForm_name").value = magnetLink.split('&').find(part => part.startsWith("dn="))?.substring(3).replace(/\+/g, ' ');
+		document.getElementById("linkForm_name").value = decodeURIComponent(magnetLink.split('&').find(part => part.startsWith("dn="))?.substring(3).replace(/\+/g, ' '));
 	}
 }
