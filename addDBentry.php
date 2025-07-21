13f3fa394d29e805f1bdf104c6b8c3e412be9592
Peter Kajan <sknitro@gmail.com>
2025-07-21 11:04:00 +0200

    Update addDBentry.php

--- addDBentry.php
+++ addDBentry.php
@@ -1,27 +1,32 @@
 <?php
-function dbquery($string, $db){
-    $db->exec("$string");
+function dbquery($entryName, $entryLink, $pubDate, $db, $table) {
+    $stmt = $db->prepare("INSERT INTO $table (title, link, pubDate) VALUES (:title, :link, :pubDate)");
+    $stmt->bindValue(':title', $entryName, SQLITE3_TEXT);
+    $stmt->bindValue(':link', $entryLink, SQLITE3_TEXT);
+    $stmt->bindValue(':pubDate', $pubDate, SQLITE3_TEXT);
+    $stmt->execute();
 }
 
 if (!empty($_POST)) {
-    $entryName = htmlspecialchars($_POST["entryName"]);
-    $entryLink = htmlspecialchars($_POST["entryLink"]);
+    $entryName = $_POST["entryName"];
+    $entryLink = $_POST["entryLink"];
     $json_data = json_decode(file_get_contents("settings.json"), TRUE);
     $dbFileName = $json_data['dbFileName'];
     $dbTableName = $json_data['tableName'];
     $db = new SQLite3($dbFileName);
 
     /* add db */
-    $pubDate = htmlspecialchars(date("D, j M Y G:i:s TP"));
-    $textquery = "INSERT INTO $dbTableName (title, link, pubDate) VALUES('$entryName', '$entryLink', '$pubDate')";
-    dbquery($textquery, $db);
-    echo "Entry:{$entryName} | URL/MAGNET:{$entryLink} was added";
+    $pubDate = date("D, j M Y G:i:s TP");
 
+    dbquery($entryName, $entryLink, $pubDate, $db, $dbTableName);
+
+    echo "Entry added: " . htmlspecialchars($entryName) . "\nMagnet: " . htmlspecialchars($entryLink);
 
     //DEBUG 
     /*$filenameR = 'DEBUG_data.txt';
     $file = fopen($filenameR, 'a');
     $queryString = "AddedDBentry: {$entryName} | {$entryLink}\n";
-    fwrite($file, $queryString);*/
+    fwrite($file, $queryString);
+    fclose($file);*/
 }
-
+?>
